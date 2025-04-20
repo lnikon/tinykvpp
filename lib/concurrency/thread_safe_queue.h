a46add05584a0b9168a46e2522b77a367e3da830
lnikon <bejanyan.vahag@protonmail.com>
2025-04-20 18:32:15 +0400

    Test reformat commit hook

--- lib/concurrency/thread_safe_queue.h
+++ lib/concurrency/thread_safe_queue.h
@@ -10,15 +10,14 @@
 
 namespace concurrency
 {
+	
 
-
-
-
-
-template <typename TItem> class thread_safe_queue_t
+template    <typename TItem> class thread_safe_queue_t
 {
   public:
-    using queue_t = std::deque<TItem>;
+    using queue_t = 
+	    
+	    std::deque<TItem>;
 
     void push(TItem item)
     {
