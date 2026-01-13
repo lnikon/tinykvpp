8f670ef640b2f231bcd93177c1b22e9f5f55ab8b
lnikon <bejanyan.vahag@pm.me>
2026-01-13 21:30:24 +0400

    Restart notice: this comment deletes everything developed so far. The latest version of the current desing can be found in feature/raft. I am going to restart the project with a new design and lessons learned.

--- scripts/build_clang_release_docker_image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build --build-arg TARGET=silkeh/clang:latest --build-arg COMPILER=clang --build-arg BUILD_TYPE=release . --file Dockerfile --tag tinykvpp-clang
--- scripts/build_gcc_release_docker_image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build --build-arg TARGET=gcc:latest --build-arg COMPILER=gcc --build-arg BUILD_TYPE=release . --file Dockerfile --tag tinykvpp-gcc
