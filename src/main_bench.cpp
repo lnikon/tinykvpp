b15970e640bf1cb8bd27ba2db8b80b6a29174436
lnikon <bejanyan.vahag@pm.me>
2026-01-16 00:59:47 +0400

    Build system setup

---
+++ src/main_bench.cpp
@@ -0,0 +1,19 @@
+#include <benchmark/benchmark.h>
+
+int SomeFunction() {
+  int a = 4;
+  int b = 5;
+  return a + b;
+}
+
+static void BM_SomeFunction(benchmark::State &state) {
+  // Perform setup here
+  for (auto _ : state) {
+    // This code gets timed
+    SomeFunction();
+  }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_SomeFunction);
+// Run the benchmark
+BENCHMARK_MAIN();
